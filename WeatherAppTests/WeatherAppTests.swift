8914a1323dba8a92260b43d7107bcf5234663e6d
Prabhjot <pgogana@promptu.com>
2022-06-26 22:59:27 +1000

    Implemented the Test Cases

--- WeatherAppTests/WeatherAppTests.swift
+++ WeatherAppTests/WeatherAppTests.swift
@@ -10,6 +10,11 @@ import XCTest
 
 class WeatherAppTests: XCTestCase {
 
+    //working
+    let json = ["id": 13213, "name": "abc", "dt": 2342] as [String : Any]
+    //failed
+    //    let json = ["":""]
+    
     override func setUpWithError() throws {
         // Put setup code here. This method is called before the invocation of each test method in the class.
     }
@@ -18,12 +23,44 @@ class WeatherAppTests: XCTestCase {
         // Put teardown code here. This method is called after the invocation of each test method in the class.
     }
 
-    func testExample() throws {
+    func testJSON() {
         // This is an example of a functional test case.
         // Use XCTAssert and related functions to verify your tests produce the correct results.
-        // Any test you write for XCTest can be annotated as throws and async.
-        // Mark your test throws to produce an unexpected failure when your test encounters an uncaught error.
-        // Mark your test async to allow awaiting for asynchronous code to complete. Check the results with assertions afterwards.
+        
+        let json = try? JSONSerialization.data(withJSONObject: self.json, options: .prettyPrinted)
+        XCTAssertNotNil(json)
+        let decoder = JSONDecoder()
+        decoder.keyDecodingStrategy = .convertFromSnakeCase
+        let decodedObject = try? decoder.decode(WeatherList.self, from: json!)
+        XCTAssertNotNil(decodedObject)
+        XCTAssert(decodedObject?.id == 13213)
+        XCTAssert(decodedObject?.name == "abc")
+        XCTAssert(decodedObject?.dt == 2342)
+        
+    }
+    
+    func testNetworkAPI() {
+        let expectation = self.expectation(description: "Valid_Request")
+        APIManager<Response>.requestData(requestURL: .list, method: .get, parameters: nil, completion: { (result) in
+            XCTAssertNotNil(result)
+            switch result {
+            case .success(let object):
+                XCTAssertNotNil(object)
+                XCTAssertNotNil(object.list)
+                XCTAssertNotNil(object.calctime)
+                expectation.fulfill()
+            case .failure(let failure) :
+                switch failure {
+                case .connectionError:
+                    XCTFail("Check your Internet connection.")
+                case .authorizationError:
+                    XCTFail("Server Error")
+                default:
+                    XCTFail("Unknown Error")
+                }
+            }
+        })
+        waitForExpectations(timeout: 5, handler: nil)
     }
 
     func testPerformanceExample() throws {
